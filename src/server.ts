1eaf98250f0ef7aa3b629729aebd2084797d0600
Maruf131 <mdsheikhmaruf131@gmail.com>
2026-07-01 16:28:12 +0600

    prisma schema design and migration

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,19 @@
 import app from "./app";
+import config from "./config";
 import { prisma } from "./lib/prisma";
-import "dotenv/config";
-const PORT = process.env.PORT;
+
+const PORT = config.port;
 
 async function main() {
   try {
-    // await prisma.$connect();
+    await prisma.$connect();
     console.log("Connected to the database");
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   } catch (error) {
     console.error("Error starting server:", error);
-    // await prisma.$disconnect();
+    await prisma.$disconnect();
     process.exit(1);
   }
 }
