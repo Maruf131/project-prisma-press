546bb8591de24d276e5b44ac1ebfa8b876f76f17
Maruf131 <mdsheikhmaruf131@gmail.com>
2026-07-02 13:09:33 +0600

    auth section user register and user login successfully done

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,17 @@ import cookieParser from "cookie-parser";
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import config from "./config";
+import { userRouter } from "./modules/user/user.route";
+import { authRouter } from "./modules/auth/auth.route";
 const app: Application = express();
 
 //middleware
-app.use(cors({
+app.use(
+  cors({
     origin: config.app_url,
     credentials: true,
-}));
+  }),
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,4 +21,8 @@ app.use(cookieParser());
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
+
+app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
+
 export default app;
---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,55 @@
+import bcrypt from "bcryptjs";
+import config from "../../config";
+import { prisma } from "../../lib/prisma";
+import { RegisterUserPayload } from "./user.interface";
+
+const registerUserIntoDB = async (payload: RegisterUserPayload) =>{
+    const { name, email, password, profilePhoto } = payload;
+    const isUserExists = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (isUserExists) {
+    throw new Error("user with this email already exists");
+  }
+
+  const hashedPassword = await bcrypt.hash(
+    password,
+    Number(config.bcrypt_salt_rounds),
+  );
+
+  const createdUser = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+      profile: {
+        create: {
+          profilePhoto,
+        },
+      },
+    },
+  });
+
+//   await prisma.profile.create({
+//     data: {
+//       userId: createdUser.id,
+//       profilePhoto,
+//     },
+//   });
+
+  const user = await prisma.user.findUnique({
+    where: { id: createdUser.id, email: createdUser.email || email },
+    omit: {
+      password: true,
+    },
+    include: {
+      profile: true,
+    },
+  });
+  return user;
+}
+
+export const userService = {
+  registerUserIntoDB,
+};
